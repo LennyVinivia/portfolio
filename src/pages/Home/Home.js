673c0c7ebdfbb77da44550489b0e42b5eff24c70
Lenny Ruprecht <lenny@vinivia.com>
2023-10-02 10:03:48 +0200

    -Adds basic layout/design of Homepage

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,5 +1,64 @@
+import { HomeStyle } from "./Home.styled";
+
 const Home = () => {
-  return <div className="home">Home</div>;
+  return (
+    <HomeStyle>
+      <main>
+        <section className="intro">
+          <h1 className="intro-title">Portfolio Website of Lenny Ruprecht</h1>
+          <p className="intro-text">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed eget
+            imperdiet nulla, non porta orci. Nunc sollicitudin nibh in tortor
+            cursus, ut auctor nulla interdum. Pellentesque habitant morbi
+            tristique senectus et netus et malesuada fames ac turpis egestas.
+            Vivamus id elit lacinia, pharetra augue id, varius mi. In commodo
+            augue non neque dignissim vestibulum.
+          </p>
+          <footer className="intro-footer">
+            <button className="scroll-hint">Scroll</button>
+          </footer>
+        </section>
+        <div className="projects-container">
+          <section className="projects-section">
+            <figure className="projects-figure">
+              <img
+                src="https://picsum.photos/1000/1000"
+                alt="project picture"
+              />
+            </figure>
+            <div className="projects-content">
+              <h2 className="project-title">Project 1</h2>
+              <p className="project-description">This is a dummy description</p>
+            </div>
+          </section>
+          <section className="projects-section">
+            <figure className="projects-figure">
+              <img
+                src="https://picsum.photos/1000/1000"
+                alt="project picture"
+              />
+            </figure>
+            <div className="projects-content">
+              <h2 className="project-title">Project 2</h2>
+              <p className="project-description">This is a dummy description</p>
+            </div>
+          </section>
+          <section className="projects-section">
+            <figure className="projects-figure">
+              <img
+                src="https://picsum.photos/1000/1000"
+                alt="project picture"
+              />
+            </figure>
+            <div className="projects-content">
+              <h2 className="project-title">Project 3</h2>
+              <p className="project-description">This is a dummy description</p>
+            </div>
+          </section>
+        </div>
+      </main>
+    </HomeStyle>
+  );
 };
 
 export default Home;
--- src/pages/Home/Home.styled.js
+++ src/pages/Home/Home.styled.js
@@ -1,3 +1,67 @@
 import styled from "@emotion/styled";
 
-export const HomeStyle = styled.div``;
+export const HomeStyle = styled.div`
+  height: 100vh;
+  overflow-x: hidden;
+
+  main {
+    padding: 50px 0px;
+    display: flex;
+  }
+  .intro {
+    position: relative;
+    min-width: 800px;
+    margin-top: 100px;
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-start;
+
+    .intro-title {
+      font-size: 80px;
+      margin-bottom: 40px;
+    }
+
+    .intro-footer {
+      display: flex;
+      width: 100%;
+      justify-content: flex-end;
+      margin-top: auto;
+
+      .scroll-hint {
+        align-self: flex-end;
+      }
+    }
+  }
+
+  .projects-container {
+    display: flex;
+    gap: 200px;
+    margin-left: 15vw;
+
+    .projects-section {
+      display: flex;
+      gap: 50px;
+
+      .projects-figure {
+        border-radius: 15px;
+        overflow: hidden;
+        height: 800px;
+        width: 800px;
+        display: block;
+        position: relative;
+
+        img {
+          object-fit: cover;
+        }
+      }
+    }
+  }
+
+  &::-webkit-scrollbar {
+    display: none;
+  }
+
+  scrollbar-width: none;
+
+  -ms-overflow-style: none;
+`;
